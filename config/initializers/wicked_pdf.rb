cb2c426684bc4bad091d8184f9d9ce118464cb6a
Regis Bruggheman <regis.bruggheman@gmail.com>
2017-01-16 17:03:38 +0100

    bump

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,6 @@
 if Rails.env == "production"
   WickedPdf.config = {
-    :exe_path => '/home/missim/ruby/gems/bin/wkhtmltopdf'
+    #:exe_path => '/home/missim/ruby/gems/bin/wkhtmltopdf'
+    :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
   }
 end
